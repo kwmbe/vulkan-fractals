78bf4de5d313cad8d69d4b81ee4dc11fc2178574
kwmbe <koberkein@gmail.com>
2025-12-10 18:58:21 +0100

    Instance

--- main.cpp
+++ main.cpp
@@ -22,6 +22,9 @@ public:
   }
 
 private:
+  vk::raii::Context  context;
+  vk::raii::Instance instance = nullptr;
+
   GLFWwindow* window = nullptr;
 
   void initWindow() {
@@ -33,7 +36,44 @@ private:
   }
 
   void initVulkan() {
+    createInstance();
+  }
 
+  void createInstance() {
+    constexpr vk::ApplicationInfo appInfo{
+      .pApplicationName =   "Hello Triangle",
+      .applicationVersion = VK_MAKE_VERSION( 1, 0, 0 ),
+      .pEngineName =        "No Engine",
+      .engineVersion =      VK_MAKE_VERSION( 1, 0, 0 ),
+      .apiVersion =         vk::ApiVersion
+    };
+
+    uint32_t glfwExtensionCount = 0;
+    auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    auto extensionProperties = context.enumerateInstanceExtensionProperties();
+
+    std::cout << "available extensions:\n";
+
+    for (const auto& extension : extensionProperties) {
+        std::cout << '\t' << extension.extensionName << '\n';
+    }
+
+    for (uint32_t i = 0; i < glfwExtensionCount; ++i) {
+      if (std::ranges::none_of(
+        extensionProperties,
+        [glfwExtension = glfwExtensions[i]] (auto const& extensionProperty) { return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }
+      )) {
+        throw std::runtime_error("Required GLFW extensions not supported: " + std::string(glfwExtensions[i]));
+      }
+    }
+
+    vk::InstanceCreateInfo createInfo{
+      .pApplicationInfo =        &appInfo,
+      .enabledExtensionCount =   glfwExtensionCount,
+      .ppEnabledExtensionNames = glfwExtensions
+    };
+
+    instance = vk::raii::Instance(context, createInfo);
   }
 
   void mainLoop() {
